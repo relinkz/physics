d1e6b4e732cf8778ebe491ede3dce16000e37f79
Apakakan <apakaka@live.com>
2016-10-04 09:19:40 +0200

    physics namespace

---
+++ PhysicsApp/Physics.cpp
@@ -0,0 +1,18 @@
+#include "Physics.h"
+
+
+void Physics::doPhysics(/*Body body1, Body body2*/)
+{
+	//do c00l stuff
+}
+
+float Physics::calcSpeed(/*Body body*/)
+{
+	float result = 0;
+	
+	//result += abs(body.getVelocity().x);
+	//result += abs(body.getVelocity().y);
+	//result += abs(body.getVelocity().z);
+
+	return result;
+}
---
+++ PhysicsApp/Physics.h
@@ -0,0 +1,14 @@
+#ifndef PHYSICS_H
+#define PHYSICS_H
+#include "GlobalData.h"
+
+
+namespace Physics
+{
+	void doPhysics(/*Body body1, Body body2 */);
+	float calcSpeed(/*Body body*/);
+
+
+}
+
+#endif
