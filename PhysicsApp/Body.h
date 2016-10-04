04551c09652d8a53fb55abe595279bd8df9911f2
relinkz <seb.lundgren@gmail.com>
2016-10-04 09:20:49 +0200

    formot to save project, so here is more data

--- PhysicsApp/Body.h
+++ PhysicsApp/Body.h
@@ -1,5 +1,6 @@
 #ifndef BODY_H
 #define BODY_H
+#include "Model.h"
 #include "GlobalData.h"
 
 
@@ -7,13 +8,32 @@ class Body
 {
 private:
 	std::string name;
-	Vector3 Velocity;
-	float Mass;
-	float Size;
-
+	Vector3 velocity;
+	Vector3 position;
+	float mass;
+	float size;
+	Model* bodyModel;
 public:
 	Body();
+	Body(Model* src, const Vector3 &pos);
 	virtual ~Body();
+
+	std::string getName() const;
+	Vector3 getVelocity() const;
+	Vector3 getPosition() const;
+	float getMass() const;
+	float getSize() const;
+	Model* getModelAdress();
+
+	void setName(const std::string& newName);
+	void setVelocity(const Vector3& newVelocity);
+	void setPosition(const Vector3& newPosition);
+	void setMass(const float& newMass);
+	void setSize(const float& newSize);
+	void setModelAdress(Model* newAdress);
+
+
+
 };
 
 #endif
