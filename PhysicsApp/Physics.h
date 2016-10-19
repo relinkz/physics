1b8f24a206964ca48a931b13afe364d4aff2ae1c
relinkz <seb.lundgren@gmail.com>
2016-10-19 11:11:51 +0200

    included .h file aswell

--- PhysicsApp/Physics.h
+++ PhysicsApp/Physics.h
@@ -6,7 +6,7 @@
 
 namespace Physics
 {
-	void atracttion(Body &body1, Body &body2, int time);
+	void attraction(Body &body1, Body &body2, int time);
 	long float calcRadius(const Vector3& v1);
 	Vector3 normalizeVector(Vector3 vec);
 }
